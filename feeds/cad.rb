8d6633e325c20ddce8dd2af7cf61f0c048219ee8
tourn <latzer.daniel@gmail.com>
2017-08-27 20:59:52 +0200

    Restore feeds

--- feeds/cad.rb
+++
@@ -1,6 +0,0 @@
-require 'feed_ninja'
-
-get "http://www.tabletitans.com/feed" do
-    title_matches /^Tales/
-      text_at "//section[@class='tales']/article/p"
-end
--- feeds/dilber.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-require 'feed_ninja'
-
-get "http://dilbert.com/feed" do
-  picture_at "//img[@class='img-comic']/@src"
-end
-
