54ec0c956e77712b5a31154f513c0f34b0bb274b
tourn <latzer.daniel@gmail.com>
2017-08-29 18:37:27 +0200

    Fix CAD

--- feeds/cad.rb
+++ feeds/cad.rb
@@ -3,6 +3,6 @@
 require "feed_ninja"
 
 get "http://cad-comic.com/?feed=rss" do
-  picture_at '//*[@id="background"]/div[2]/div[3]/article/div[2]/a[2]/img/@src'
+  picture_at '//*[@class="comicpage"]/a[not(@rel)]/img/@src'
   text_at '//*[@id="comicblog"]/div[2]'
 end
